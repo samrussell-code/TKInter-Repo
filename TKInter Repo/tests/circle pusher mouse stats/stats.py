d016a9de6f1a3e700d3a87b6aaf3f07b957c39a3
samrussell-code <66648381+samrussell-code@users.noreply.github.com>
2021-05-20 23:37:10 +0100

    montys epic bug fix

--- TKInter Repo/tests/circle pusher mouse stats/stats.py
+++ TKInter Repo/tests/circle pusher mouse stats/stats.py
@@ -14,27 +14,35 @@ class SimulationWindow(Tk):
         self.x,self.y,self.width,self.height,self.oldx,self.oldy,self.mX,self.mY,self.TIME=0,0,1000,1000,0.01,0.01,0.01,0.01,0.01
         self.canvas=self.box=Canvas(self,width=self.width-20,height=self.height-20,bg="#DDFFF7")
         self.canvas.place(x=10,y=10)
-        bg_process=threading.Thread(target=self.CheckVelocity)
+        self.update()      
+        self.start_threads() #must start in a function
+
+    def start_threads(self):
+        t1 = threading.Thread(target=self.Process1,daemon=True);t1.start()#t1.start is important
+        t2 = threading.Thread(target=self.Process2,daemon=True);t2.start()
+
+
+
+    def Process1(self):
         while True:
             print("process1")
             self.update()
             self.canvas.bind('<Motion>',self.MouseMotion)
-            bg_process.start()
-            bg_process.join()
 
-
-
-            self.CheckVelocity()
-            #self.after(int((1/self.TIME)*10**3),self.CheckVelocity)
     def MouseMotion(self,event):
         self.mX,self.mY=event.x,event.y
-    def CheckVelocity(self):
-        print("process2")
-        time.sleep(self.TIME)
-        distance=sqrt(((self.mY-self.oldy)**2)+((self.mX-self.oldx)**2))
-        velocity=distance/self.TIME
-        self.oldy,self.oldx=self.mY,self.mX
-        print(velocity)
+
+    def Process2(self):
+        while True:
+            print("process2")
+            time.sleep(self.TIME)
+            distance=sqrt(((self.mY-self.oldy)**2)+((self.mX-self.oldx)**2))
+            velocity=distance/self.TIME
+            self.oldy,self.oldx=self.mY,self.mX
+            print(velocity)
+
+    
+
 
 window=SimulationWindow()
 window.mainloop()
